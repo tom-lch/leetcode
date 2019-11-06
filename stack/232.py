cddf4ce47151a48758b20243021054ea9dece5a9
Samuel <lichao_cs@163.com>
2019-11-06 20:17:31 +0800

    使用双栈来模拟队列，效率极其低下

---
+++ stack/232.py
@@ -0,0 +1,73 @@
+"""
+使用栈实现队列的下列操作：
+
+push(x) -- 将一个元素放入队列的尾部。
+pop() -- 从队列首部移除元素。
+peek() -- 返回队列首部的元素。
+empty() -- 返回队列是否为空。
+示例:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);
+queue.peek();  // 返回 1
+queue.pop();   // 返回 1
+queue.empty(); // 返回 false
+说明:
+
+你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class MyQueue:
+    # 在数据结构中，使用双栈来解决队列的问题
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.instack = []
+        self.outstack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.instack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if len(self.outstack) == 0:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if len(self.outstack) == 0:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.instack) == 0 and len(self.outstack) == 0
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
