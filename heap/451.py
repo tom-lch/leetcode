96c4d090b9335ebc88cec3a1a3e7f19e38ede0a4
Samuel <lichao_cs@163.com>
2019-11-07 19:59:23 +0800

    中等 使用python的方法写的一团糟

---
+++ heap/451.py
@@ -0,0 +1,33 @@
+"""
+给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+
+示例 1:
+
+输入:
+"tree"
+
+输出:
+"eert"
+
+解释:
+'e'出现两次，'r'和't'都只出现一次。
+因此'e'必须出现在'r'和't'之前。此外，"eetr"也是一个有效的答案。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-characters-by-frequency
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        S = ''
+        print(s)
+        ss = list(set(list(s)))
+        zz = []
+        for _ in ss:
+            zz.append(0)
+        dict_s = dict(zip(ss, zz))
+        for i in s:
+            dict_s[i] += 1
+        for i in sorted(dict_s, key=dict_s.__getitem__, reverse=True):
+            S += i*dict_s[i]
+        return S
