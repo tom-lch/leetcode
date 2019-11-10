30e8f3d3f04c594d083ad5431a8f35a26c673bb3
Samuel <lichao_cs@163.com>
2019-11-10 21:18:11 +0800

    简单 二叉搜索树中的搜索

---
+++ tree/700.py
@@ -0,0 +1,55 @@
+"""
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+
+例如，
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和值: 2
+你应该返回如下子树:
+
+      2
+     / \
+    1   3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        # Definition for a binary tree node.
+        # class TreeNode:
+        #     def __init__(self, x):
+        #         self.val = x
+        #         self.left = None
+        #         self.right = None
+
+        class Solution:
+            def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+                # 递归
+                if not root or val == root.val:
+                    return root
+                return self.searchBST(root.left, val) if val<root.val else self.searchBST(root.right, val)
+                '''
+                # 迭代
+                while True:
+                    if not root or val == root.val:
+                        return root
+                    root = root.left if root.val>val else root.right
+
+                '''
